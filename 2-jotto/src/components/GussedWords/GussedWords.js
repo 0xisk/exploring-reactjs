e0dc58ad94c5b66a661addd4c9d73c0f4aafb092
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 16:38:29 +0000

    Initializing an initial starter for GussedWords component, 2-jotto.

---
+++ 2-jotto/src/components/GussedWords/GussedWords.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+const GussedWords = (props) => {
+  return (
+    <div />
+  )
+};
+
+GussedWords.propTypes = {
+
+};
+
+export default GussedWords;
