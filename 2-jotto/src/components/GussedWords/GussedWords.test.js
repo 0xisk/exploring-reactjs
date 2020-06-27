7d0c5a2ab147ad25bdd3ab80aede6cf1f8bba93a
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 18:13:00 +0000

    Adding some test suites for GuessedWords and getting a red results, 2-jotto.

--- 2-jotto/src/components/GussedWords/GussedWords.test.js
+++ 2-jotto/src/components/GussedWords/GussedWords.test.js
@@ -27,14 +27,27 @@ test('does not throw warning with default props', () => {
 });
 
 describe('if there are words gussed', () => {
-  test('renders without error', () => {
+  let wrapper;
+  let guessedWords = [
+    { guessedWords: 'train', letterMatchCount: 3 },
+    { guessedWords: 'agile', letterMatchCount: 1 },
+    { guessedWords: 'party', letterMatchCount: 5 }
+  ];
 
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
   });
   test('renders "guessed words" section', () => {
-
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
   });
   test('correct number of guessed words', () => {
-
+    const guessedWordNode = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNode.length).toBe(guessedWords.length);
   });
 });
 
