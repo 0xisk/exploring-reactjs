c8309210f3fa9ae0b76ee033a85579b57f5e4bbf
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 17:21:06 +0000

    Split GussedWords testing to two describes and initialize first red test function, 2-jotto.

--- 2-jotto/src/components/GussedWords/GussedWords.test.js
+++ 2-jotto/src/components/GussedWords/GussedWords.test.js
@@ -25,3 +25,18 @@ const setup = (props={}) => {
 test('does not throw warning with default props', () => {
   checkProps(GussedWords, defaultProps);
 });
+
+describe('if there are words gussed', () => {
+  test('renders without no errors', () => {
+    const wrapper = setup({ gussedWords: [] });
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+
+  });
+});
+
+describe('if there are no words gussed', () => {
+
+});
