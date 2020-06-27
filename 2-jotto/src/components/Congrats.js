5c8e7bc574a96655be3b5accc287f6ffad05e7eb
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 03:04:24 +0000

    Adding Congrats component, 2-jotto.

---
+++ 2-jotto/src/components/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'ract';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function 
+ * @returns {JSX.Element} - Rendered Comonent (or null if 'success' prop is false).
+ */
+export default () => {
+  return (
+    <div />
+  );
+}
