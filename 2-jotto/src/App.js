c45742a643039b0594cc34c93c2b762eaf3d053c
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 18:57:45 +0000

    Making an Acceptance testing by adding GuessedWords and Congrats into App component, 2-jotto.

--- 2-jotto/src/App.js
+++ 2-jotto/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Congrats from './components/Congrats/Congrats';
+import GuessedWords from './components/GussedWords/GussedWords';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWords: 'train', letterMatchCount: 3 }
+        ]}/>
+      </div>
+    );
+  }
 }
 
 export default App;
