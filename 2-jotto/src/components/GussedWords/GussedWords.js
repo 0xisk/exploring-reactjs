bbde74979bb46fe4aed73154349781d3fe229854
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 17:38:07 +0000

    Test the second describe for GuessedWords with green results, 2-jotto.

--- 2-jotto/src/components/GussedWords/GussedWords.js
+++ 2-jotto/src/components/GussedWords/GussedWords.js
@@ -8,8 +8,19 @@ import PropTypes from 'prop-types';
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 const GussedWords = (props) => {
+  let contents;
+  
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test='guess-instructions'>
+        Try to guess the secret word!
+      </span>
+    );
+  }
+
   return (
     <div data-test="component-guessed-words">
+      {contents}
     </div>
   )
 };
