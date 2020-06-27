d377d0eeb2e4c7c06edb4d7a7d38906613a2b6f1
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 03:13:18 +0000

    Initializing a some test functions, 2-jotto.

--- 2-jotto/src/components/Congrats.js
+++ 2-jotto/src/components/Congrats.js
@@ -1,4 +1,4 @@
-import React from 'ract';
+import React from 'react';
 
 /**
  * Functional react component for congratulatory message.
--- 2-jotto/src/components/Congrats.test.js
+++ 2-jotto/src/components/Congrats.test.js
@@ -5,3 +5,15 @@ import EnzymeAdaptor from 'enzyme-adapter-react-16';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdaptor() });
+
+test('renders without errors', () => {
+
+});
+
+test('renders no text when `success` prop is false', () => {
+
+});
+
+test('renders non-empty congrats message when `success` prop is false', () => {
+
+});
