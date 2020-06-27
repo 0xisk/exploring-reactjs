26d2cf8447979d99471dd8548c1baa5f716ee42e
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 18:59:59 +0000

    Adding Bootstap styles into App.css, 2-jotto.

--- 2-jotto/src/App.js
+++ 2-jotto/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Congrats from './components/Congrats/Congrats';
 import GuessedWords from './components/GussedWords/GussedWords';
