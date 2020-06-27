6c1c1fdbc31f40ebead2670ae690e3e4ece92fb7
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-06-27 16:36:36 +0000

    Implementing first test function for GussedWords components, 2-jotto.

--- 2-jotto/src/components/GussedWords/GussedWords.test.js
+++ 2-jotto/src/components/GussedWords/GussedWords.test.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 
 import { findByTestAttr, checkProps } from '../../utils/testUtils';
 import GussedWords from './GussedWords';
 
+const defaultProps = [{
+  gussedWords: [{
+    gussedWords: 'train',
+    letterMatchCount: 3
+  }]
+}];
+
+/**
+ * Factory function to create a shallowWrapper for the GussedWords component. 
+ * @function setup
+ * @param {object} props
+ * @returns {ShallowWrapper} 
+ */
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GussedWords {...setupProps} />);
+}
+
+test('does not throw warning with default props', () => {
+  checkProps(GussedWords, defaultProps);
+});
