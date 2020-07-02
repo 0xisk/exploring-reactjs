6aa981fa87547a583f9b16261136a6d8038624f9
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-02 16:17:57 +0000

    Initializing an empty tests for testing successReducer function, 3-jotto-redux.

---
+++ 3-jotto-redux/src/redux/reducers/successReducer.test.js
@@ -0,0 +1,10 @@
+import { actionTypes } from '../actions';
+import { successReducer } from './successReducer';
+
+test('return default initial state of `false` when no action is paaed', () => {
+
+});
+
+test('returns state of true upon receiving an action of type `CORRECT_GUESS`', () => {
+
+});
