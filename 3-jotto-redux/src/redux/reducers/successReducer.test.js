2c1ea38165f9285bdde1667373e98b8d7bb191b9
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-02 16:23:48 +0000

    Implementing two test for successReducer with RED results, 3-jotto-redux.

--- 3-jotto-redux/src/redux/reducers/successReducer.test.js
+++ 3-jotto-redux/src/redux/reducers/successReducer.test.js
@@ -1,10 +1,12 @@
 import { actionTypes } from '../actions';
-import { successReducer } from './successReducer';
+import successReducer from './successReducer';
 
 test('return default initial state of `false` when no action is paaed', () => {
-
+  const newState = successReducer();
+  expect(newState).toBe(false);
 });
 
 test('returns state of true upon receiving an action of type `CORRECT_GUESS`', () => {
-
+  const newState = new successReducer(undefined, { type: actionTypes.CORRECT_GUESS });
+  expect(newState).toBe(true); 
 });
