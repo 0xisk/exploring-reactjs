4eab2f373065df837dd52be9c550f1332f0f832e
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-02 19:35:21 +0000

    Implementing an empty tests for testing InputComponent, 2-jotto-redux.

--- 3-jotto-redux/src/components/Input/input.test.js
+++ 3-jotto-redux/src/components/Input/input.test.js
@@ -4,6 +4,31 @@ import { shallow } from 'enzyme';
 import { findByTestAttr } from '../../utils/testUtils';
 import Input from './Input';
 
-test('', () => {
+describe('render', () => {
+  decribe('word has not been guessed', () => {
+    test('renders component wihout error', () => {
+
+    });
+    test('renders input box', () => {
+
+    });
+    test('renders submit button', () => {
+
+    });
+  });
+  decribe('word has been guessed', () => {
+    test('renders component wihout error', () => {
+
+    });
+    test('does not renders input box', () => {
+
+    });
+    test('does not renders submit button', () => {
+
+    });
+  });
+});
+
+describe('update state', () => {
 
 });
