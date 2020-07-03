5135578d68508c165f438971595ea3fe410b4b6a
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 14:18:15 +0000

    Implementing guessedWords() reducer function, 3-jotto-redux.

--- react-testing/3-jotto-redux/src/redux/actions/index.js
+++ react-testing/3-jotto-redux/src/redux/actions/index.js
@@ -3,7 +3,13 @@ export const actionTypes = {
   GUESS_WORD: 'GUESS_WORD'
 };
 
-
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
+ */
 export const guessWord = (guessedWord) => {
   return function(dispatch, getState) {
 
