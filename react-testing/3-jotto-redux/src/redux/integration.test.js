1ba7df54dd7ca0133ed81e792ed5f206312e50dd
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 14:08:12 +0000

    Implementing an intiail integration testing for Redux, 3-jotto-redux.

---
+++ react-testing/3-jotto-redux/src/redux/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../utils/testUtils';
+import { guessedWord } from './actions';
+
+describe('guessed action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+  describe('some guessed words', () => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+}); 
