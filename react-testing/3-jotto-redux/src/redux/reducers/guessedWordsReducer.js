1eba1e0853f5156186fd4866c401070c92d06ff3
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-03 14:58:20 +0000

    Implementing guessedWords() reducer function, 3-jotto-redux.

--- react-testing/3-jotto-redux/src/redux/reducers/guessedWordsReducer.js
+++ react-testing/3-jotto-redux/src/redux/reducers/guessedWordsReducer.js
@@ -1,9 +1,16 @@
+import { actionTyps } from '../actions';
+
 /**
  * @function guessedWordsReducers
  * @param {array} state - Array of guessed words.
  * @param {object} action - Action to be reduced.
  * @returns {array} - New guessedWords state.
  */
-export default (state, action) => {
-  return null;
+export default (state=[], action) => {
+  switch(action.type) {
+    case action.type.GUESS_WORD:
+      return [...state, action.payload];
+    default: 
+      return state;
+  }
 }
