86a8bd2320e5bd84f9b37b426b5161678ef19f18
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 21:10:32 +0000

    Implementing App component, 3-jotto-redux.

--- react-testing/3-jotto-redux/src/App.js
+++ react-testing/3-jotto-redux/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-
-import Congrats from './components/Congrats/Congrats';
-import GuessedWords from './components/GussedWords/GussedWords';
+import { connect } from 'react-redux';
 import './App.css';
 
-export class App extends Component {
+import GuessedWords from './redux/reducers/guessedWordsReducer';
+import Congrats from './components/Congrats/Congrats';
+import Input from './components/Input/Input';
+import { getSecretWord } from './redux/actions/index';
+
+export class UnconnectedApp extends Component {
+  /**
+   * @method componentDidMount
+   * @returns {undefined}
+   */
+  componentDidMount() {
+    // get the secret word
+    this.props.getSecretWord();
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[
-          { guessedWords: 'train', letterMatchCount: 3 }
-        ]}/>
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
