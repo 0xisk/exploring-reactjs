fc3be9867620614d7e720c19336d1fcc1dd3352b
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-07-09 21:08:36 +0000

    Initialize setup() function to test App component, 3-jotto-redux.

--- react-testing/3-jotto-redux/src/App.test.js
+++ react-testing/3-jotto-redux/src/App.test.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import { findByTestAttr, storeFactory } from './utils/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  
-});
+
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWord
+ * @function setup
+ * @param {object} initialState 
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />).dive().dive();
+
+  return wrapper;
+}
